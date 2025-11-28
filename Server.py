6ebbf0994a75ed3212f3586e317f9414fb5b52b4
Anton <antonoo@metropolia.fi>
2025-11-28 18:35:33 +0200

    alkujuttuja

---
+++ Server.py
@@ -0,0 +1,35 @@
+from flask import Flask, request
+from flask_cors import CORS
+import json
+import secrets
+
+players = {}
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/game", methods=["POST"])
+def new_game():
+    args = request.args
+    name = args.get('name')
+    id = secrets.randbits(64)
+    return {'status': None, 'name': name,  'id': id}
+
+
+
+@app.route("/game", methods=["PUT"])
+def update_game():
+    args = request.args
+    id = args.get('id')
+    action = args.get('action')
+    return {'status': None}
+
+@app.route("/game", methods=["GET"])
+def game_status():
+    args = request.args
+    id = args.get('id')
+    return {'status': None}
+    
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
